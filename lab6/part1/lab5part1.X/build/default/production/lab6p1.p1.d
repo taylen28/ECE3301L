9b45aed3d592dba40a0408b53b6c546e693e537b
Taylen Vo <taylenvo23@gmail.com>
2025-10-09 16:01:05 -0700

    d

--- lab6/part1/lab5part1.X/build/default/production/lab6p1.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/lab6p1.d  \
- build/default/production/lab6p1.p1:  \
- lab6p1.c 
--- lab6/part1/lab6part3.X/build/default/production/lab6p3.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/lab6p3.d  \
- build/default/production/lab6p3.p1:  \
- lab6p3.c 
